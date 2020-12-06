c71f86fabc11bf28cdae60b3accd500626d42dcf
thexdev <akbarnugroho253@gmail.com>
2020-12-07 00:39:28 +0700

    :seedling: remove misused factory at user seeder

--- database/seeds/UserSeeder.js
+++ database/seeds/UserSeeder.js
@@ -15,7 +15,6 @@ const Factory = use('Factory')
 
 class UserSeeder {
   async run () {
-    await Factory.model('App/Models/User').create()
     await Factory.model('App/Models/User').create({ is_admin: false })
   }
 }
