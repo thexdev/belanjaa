80b6517a320dc42cb1228d9d9ace785df50553f3
thexdev <akbarnugroho253@gmail.com>
2020-12-07 00:28:53 +0700

    :seedling: create seeder for users with type admin

---
+++ database/seeds/AdminSeeder.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| AdminSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class AdminSeeder {
+  async run () {
+    await Factory.model('App/Models/User').create({ is_admin: true })
+  }
+}
+
+module.exports = AdminSeeder
