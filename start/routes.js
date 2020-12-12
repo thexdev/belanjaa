90a93ac0d15cd78b224195fc4ef04a6c3f302338
M. Akbar Nugroho <37355855+thexdev@users.noreply.github.com>
2020-12-12 19:32:28 +0700

    :sparkles: add routes * add sign-in routes
    * add homepage routes
    
    * add dashboard index routes
    
    * add manage expedition routes

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,25 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.group(() => {
+  Route.get('/signin', 'SigninController.index').as('auth.signin')
+  Route.post('/signin/attempt', 'SigninController.attempt').as('auth.attempt')
+})
+  .namespace('Auth')
+
+Route.group(() => {
+  Route.get('/', 'HomeController.index').as('frontend.home')
+})
+  .namespace('Frontend')
+
+Route.group(() => {
+  Route.get('/dashboard', 'DashboardController.index').as('dashboard.index')
+
+  Route.get('/expeditions', 'ExpeditionController.index').as('dashboard.expedition.index')
+  Route.get('/expeditions/add', 'ExpeditionController.add').as('dashboard.expedition.add')
+  Route.post('/expeditions/store', 'ExpeditionController.store').as('dashboard.expedition.store')
+  Route.get('/expeditions/edit/:id', 'ExpeditionController.edit').as('dashboard.expedition.edit')
+  Route.put('/expeditions/update/:id', 'ExpeditionController.update').as('dashboard.expedition.update')
+  Route.delete('/expeditions/delete/:id', 'ExpeditionController.destroy').as('dashboard.expedition.delete')
+})
+  .namespace('Admin')
